f93da571fcbb49e4fdf593528d6260d21e438fe8
b410asakura <73512839+b410asakura@users.noreply.github.com>
2022-03-08 21:07:25 +0600

    Delete FXFXFXF directory

--- FXFXFXF/src/sample/Configs.java
+++
@@ -1,7 +0,0 @@
-package sample;
-
-public class Configs {
-    protected String url = "jdbc:mysql://localhost:3306/goshadudar";
-    protected String user = "root";
-    protected String password = "57206700";
-}
